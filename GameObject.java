dd58928d69d16c9668ac14748edfca345b4c2ac4
pavel <pavel.tapu23@gmail.com>
2023-09-12 19:02:30 +0300

    Code for Lab 1

--- GameObject.java
+++ GameObject.java
@@ -16,10 +16,4 @@ public class GameObject {
         System.out.println("Speed: " + speed + " units per second");
         System.out.println("Color: " + color);
     }
-
-    public static void main(String[] args) {
-        GameObject obj1 = new GameObject("Object 1", "moving", 10.0, "red");
-
-        obj1.displayState();
-    }
 }
---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        GameObject obj1 = new GameObject("Object 1", "moving", 10.0, "red");
+
+        obj1.displayState();
+    }
+}
