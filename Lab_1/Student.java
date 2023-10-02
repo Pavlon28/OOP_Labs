3dce7e5b5b028ef53fcb15bfb5b9ddd182f29bb1
Pavel Tapu <pavel.tapu23@gmail.com>
2023-10-02 19:34:03 +0300

    merge branch

--- Lab_1/Student.java
+++ Lab_1/Student.java
@@ -3,17 +3,7 @@ package Lab_1;
 import java.util.Date;
 
 public class Student {
-    private final String FirstName;
-    private final String LastName;
-    private final String Email;
-    private final Date EnrollmentDate;
-    private final Date DateOfBirth;
     public Student(String FirstName, String LastName, String Email, Date EnrollmentDate, Date DateOfBirth){
-        this.FirstName = FirstName;
-        this.LastName = LastName;
-        this.Email = Email;
-        this.EnrollmentDate = EnrollmentDate;
-        this.DateOfBirth = DateOfBirth;
     }
 
 }
