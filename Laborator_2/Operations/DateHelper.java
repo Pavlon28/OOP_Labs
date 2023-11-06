12e24d920cc5fc379a3990e1cef70af7deb998f7
Pavlon28 <114653759+Pavlon28@users.noreply.github.com>
2023-11-06 09:19:07 +0200

    added date helper

---
+++ Laborator_2/Operations/DateHelper.java
@@ -0,0 +1,14 @@
+package Laborator_2.Operations;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateHelper {
+
+    private static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS";
+
+    public static String formatDate(long timestamp) {
+        SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
+        return sdf.format(new Date(timestamp));
+    }
+}
