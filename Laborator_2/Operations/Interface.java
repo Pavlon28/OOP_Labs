c43810e1899dff4e11b217dcd069633da8d240ba
Pavlon28 <114653759+Pavlon28@users.noreply.github.com>
2023-11-06 22:18:03 +0200

    created the snapshot, edited the interface, and added some changes to gitignore

--- Laborator_2/Operations/Interface.java
+++ Laborator_2/Operations/Interface.java
@@ -1,8 +1,10 @@
 package Laborator_2.Operations;
 
+
 import java.util.Scanner;
 
 public class Interface {
+
     private final String folderPath;
 
     public Interface(String folderPath) {
@@ -10,7 +12,27 @@ public class Interface {
     }
 
     public void run() {
+        var detector = new FileDetector(folderPath);
 
         Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter command (commit, info, status, exit):");
+            String command = scanner.nextLine();
+            switch (command) {
+                case "commit" -> detector.commit();
+                case "info" -> {
+                    System.out.println("Enter file name:");
+                    String fileName = scanner.nextLine();
+                    detector.info(fileName);
+                }
+                case "status" -> detector.status();
+                case "exit" -> {
+                    scanner.close();
+                    System.exit(0);
+                }
+                default -> System.out.println("Invalid command.");
+            }
+        }
     }
+
 }
