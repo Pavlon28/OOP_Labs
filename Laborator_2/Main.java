14eb5f2a84368b321dab2428a51286b231da82b7
Pavlon28 <114653759+Pavlon28@users.noreply.github.com>
2023-11-06 09:45:47 +0200

    added the interface

--- Laborator_2/Main.java
+++ Laborator_2/Main.java
@@ -1,5 +1,11 @@
 package Laborator_2;
 
+import Laborator_2.Operations.Interface;
+
 public class Main {
+    public static void main(String[] args) {
+        var applicationBehaviour = new Interface("Lavorator_2/Files/");
+        applicationBehaviour.run();
+    }
     
 }
--- Laborator_2/Operations/Interface.java
+++ Laborator_2/Operations/Interface.java
@@ -1,5 +1,16 @@
 package Laborator_2.Operations;
 
+import java.util.Scanner;
+
 public class Interface {
-    
+    private final String folderPath;
+
+    public Interface(String folderPath) {
+        this.folderPath = folderPath;
+    }
+
+    public void run() {
+
+        Scanner scanner = new Scanner(System.in);
+    }
 }
